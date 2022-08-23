8e9a3c85884d13bb42a2aed56135c02a698fb0a2
Thomas von Loh <Thomas.von-Loh@adesso.de>
2022-08-23 14:39:39 +0200

    yargs title body

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,24 +3,62 @@
 // or: nodemon app.js
 // end nodemon: strg + c
 const validator = require('validator')
-const yargs = require('yargs')
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
+const yargs = require('yargs')
 
+// Customize yargs version
 yargs.version('1.1.0')
 
-const msg = getNotes()
-const error = chalk.bold.hex('#DEADED').italic
-const information = chalk.bold.cyanBright.bgWhite
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Notes title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'add Body Description',
+      demandOption: true,
+      type: 'string',
+    }
+  },
+  handler: function (argv) {
+    console.log('Title: ' + argv.title + '; ', 'Body: ' + argv.body);
+  }
+})
 
-console.log(msg)
-console.log(information(validator.isEmail('fips.froelich@gmail.com')))
-console.log(error(validator.isURL('https://www.nope.de')))
-console.log(process.argv);
-console.log(process.argv[2]);
-console.log(process.argv[3]);
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Remove command');
+  }
+})
 
-// node app.js --title="Things to by"
-// node app.js --help
-// node app.js --version
-console.log(yargs.argv);
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'Show list',
+  handler: function () {
+    console.log('list');
+  }
+})
+
+// Create read commands
+yargs.command({
+  command: 'read',
+  describe: 'read  note',
+  handler: function () {
+    console.log('Read note');
+  }
+})
+
+//you need to execute yargs once  
+yargs.parse()
+
+// console.log(yargs.argv);

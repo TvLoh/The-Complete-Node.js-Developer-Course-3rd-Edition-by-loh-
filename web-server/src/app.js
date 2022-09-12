d1176f894bf72302e111922d79c9986caec757a0
Thomas von Loh <Thomas.von-Loh@adesso.de>
2022-09-12 09:54:57 +0200

    47. Dynamic Pages with heandlebar 'hbs' * <h1>{{ message }}<h1/>

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,22 @@ const express = require('express')
 const app = express()
 const publicDiractoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDiractoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {title: 'Weather App', userName: 'yourName'})
+}) 
+
+app.get('/abought', (req, res) => {
+  res.render('abought', {title: 'Abought Me', userName: 'yourName'})
+}) 
+
+app.get('/help', (req, res) => {
+  res.render('help', {helpMessage: "This is a help Message"})
+}) 
+
+
 
 app.get('/weather', (req, res) => {
   res.send('Hier könnte Ihr Wetter angezeigt ')

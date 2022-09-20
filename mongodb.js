05de4f7e31d445781fa4424d5b37efa369543049
Thomas von Loh <Thomas.von-Loh@adesso.de>
2022-09-20 14:40:07 +0200

    First Steps into MongoDB *find({name: 'Name'}).toArray((err, user) => {...}) .countDocuments({name: 'Name'}) => count of entries

--- mongodb.js
+++ mongodb.js
@@ -1,73 +1,38 @@
 // CRUD create read update delete delete
 
-const {MongoClient, ObjectId} = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 const id = new ObjectId()
-console.log(id);
 
-
-MongoClient.connect(connectionURL, {useNewUrlParser:true}, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) {
     return console.log(error)
   }
 
   const db = client.db(databaseName)
-
-/*   db.collection('users').insertOne({
-    userName: 'myFirstName',
-    userAge: 27
-  }, (err, result) => {
-    if (err) {
-      console.log('Unable to insert User');
-    }
-
-    console.log(result.ops);
-  })
-
-  console.log('conected') */
 /* 
-  db.collection('users').insertMany([
-    {
-      userName: 'myFirstName', 
-      userAge: 17,
-    },
-    {
-      userName: 'newName', 
-      userAge: 26,
+  db.collection('users').findOne({_id: new ObjectId('6329a24b5986f93f26ff54ca')}, (err, collection) =>{
+    if (err) {
+      return console.log('error');
     }
-  ]), (error, result) => {
-    if (error) {
-      console.log('Fehler');
-    }
-    console.log(result.ops);
-  } */
+    console.log(collection);
+    console.log(collection._id.getTimestamp());
+  }) */
 
-
-  console.log('Before collection');
-  db.collection('documents').insertMany([
-    {
-      description: 'This is doc 1', 
-      completed: false,
-    },
-    {
-      description: 'This is doc 2', 
-      completed: false,
-    },
-    {
-      description: 'This is doc 3', 
-      completed: true,
+/*   db.collection('users').find({age: 27}).toArray((err, users) => {
+    if (err) {
+      console.log(err);
     }
-  ],{ 
-    wtimeout: 500,
-  }, (error, result) => {
-    if (error) {
-      console.log('Fehler');
-    }
-    console.log(result);
-  }
-  )
+    console.log(users);
+  }) */
 
+  db.collection('users').countDocuments({age: 27}, (err, users) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(users);
+  })
 })
